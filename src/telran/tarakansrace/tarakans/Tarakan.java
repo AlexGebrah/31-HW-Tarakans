4e440af2b23091917a7494c0e19537cbb68775d7
HPPro <alexisgebrah@gmail.com>
2025-04-28 12:49:20 +0300

    first commit

--- src/telran/tarakansrace/tarakans/Tarakan.java
+++ src/telran/tarakansrace/tarakans/Tarakan.java
@@ -2,7 +2,7 @@ package telran.tarakansrace.tarakans;
 
 import java.util.Random;
 
-public class Tarakan implements Runnable{
+public class Tarakan implements Runnable {
     private int number;
     private int distance;
     private static boolean winner = false;
@@ -16,17 +16,18 @@ public class Tarakan implements Runnable{
     public void run() {
         System.out.println("Tarakan#" + (number + 1) + " has started");
         for (int i = 0; i < distance; i++) {
-                       System.out.println("Tarakan#" + (number + 1) + " at ring " + (i + 1));
+            System.out.println("Tarakan#" + (number + 1) + " at ring " + (i + 1));
             try {
                 Thread.sleep(2 + new Random().nextInt(4));
-                System.out.println("Tarakan#" + (number + 1) + " has finished");
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
+        System.out.println("Tarakan#" + (number + 1) + " has finished");
         if (!winner) {
             winner = true;
             System.out.println("Congratulations to Tarakan#" + (number + 1) + " (winner)");
         }
+
     }
 }
